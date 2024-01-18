3d10420f0724520c7c9b1e5416fac65dffc8c219
khushi kandoi <khushikandoii09@gmail.com>
2024-01-18 23:17:15 +0530

    added 404 page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,13 @@
-
+import bgImg from "../assets/images/404pagebg.gif";
+import {Link} from "react-router-dom"
 const PageNotFound = () => {
   return (
-    <>
-      404
-    </>
+      <div className="flex h-full w-full overflow-hidden items-center justify-center" style={{backgroundImage : `url(${bgImg})`,backgroundRepeat: 'no-repeat',backgroundSize: 'cover',}}>
+        <div className=" sm:w-[600px] max-sm:w-72 flex items-center flex-col gap-4 z-10 backdrop-sepia-0  p-4 font-bold rounded-lg bg-white/50 ">
+          <p className="sm:text-md  text-slate-700 tracking-wide">Oops! The page went on a little adventure. Go back to home. </p>
+          <Link to="/" className="p-2 px-4 bg-slate-600 rounded-xl text-white ">Home</Link>
+        </div>
+      </div>
   )
 }
 
