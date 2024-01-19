2741bfbc3051b7adaaf6ba18c98c7fa11c3df99d
khushi kandoi <khushikandoii09@gmail.com>
2024-01-19 18:09:39 +0530

    modified login page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,14 +1,35 @@
-import bgImg from "../assets/images/404pagebg.gif";
-import {Link} from "react-router-dom"
+import bgImg from "../assets/images/404pagespacebg.jpg";
+import astronaut from "../assets/images/astronaut.gif";
+import { Link } from "react-router-dom";
+import notFound from "../assets/images/404png.png";
+
 const PageNotFound = () => {
   return (
-      <div className="flex h-full w-full overflow-hidden items-center justify-center" style={{backgroundImage : `url(${bgImg})`,backgroundRepeat: 'no-repeat',backgroundSize: 'cover',}}>
-        <div className=" sm:w-[600px] max-sm:w-72 flex items-center flex-col gap-4 z-10 backdrop-sepia-0  p-4 font-bold rounded-lg bg-white/50 ">
-          <p className="sm:text-md  text-slate-700 tracking-wide">Oops! The page went on a little adventure. Go back to home. </p>
-          <Link to="/" className="p-2 px-4 bg-slate-600 rounded-xl text-white ">Home</Link>
-        </div>
+    <div
+      className="flex h-full w-full max-sm:flex-col overflow-hidden items-center justify-center object-cover object-center"
+      style={{
+        backgroundImage: `url(${bgImg})`,
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className=" sm:w-[600px] max-sm:w-80 flex items-center flex-col gap-4 ">
+        <img className="flex w-96 max-sm:w-72 max-sm:h-64 h-72" src={`${notFound}`}></img>
+        <p className="sm:text-sm text-white font-bold tracking-wide ">
+          It seems like the page that you are looking for is no longer here.
+        </p>
+        <Link to="/" className="p-3 px-10 font-bold text-xs bg-violet-800 rounded-full text-white hover:cursor-pointer">
+          TAKE ME HOME
+        </Link>
       </div>
-  )
-}
+
+      <div
+      className="flex w-1/2 h-1/2 max-sm:hidden"
+      style={{
+        backgroundImage: `url(${astronaut})`,
+        backgroundRepeat: "no-repeat",
+      }} ></div>
+    </div>
+  );
+};
 
 export default PageNotFound;
