4b88ffbac4d92d32615114b734e158ee638b05a7
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-21 12:16:44 +0530

    made code better,readable and less redundant

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -3,38 +3,13 @@ import { logoutAsync } from "../slices/authSlice";
 import { useEffect, useState } from "react";
 import { Navigate, useNavigate, Link } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
+import { navigation } from "../constants/navigation";
 
 const Statistics = () => {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
   const navigate = useNavigate();
-  const [open, setOpen] = useState(false);
 
-  const navigation = [
-    {
-      name: "Dashboard",
-      link: "/",
-      icon: "ri-dashboard-line",
-    },
-    {
-      name: "Statistics",
-      link: "/",
-      icon: "ri-bar-chart-box-line",
-    },
-    { name: "Chats", link: "/", icon: "ri-chat-3-line" },
-    { name: "My Wallet", link: "/", icon: "ri-wallet-line" },
-    {
-      name: "Transfers",
-      link: "/",
-      icon: "ri-arrow-up-down-line",
-    },
-    { name: "Settings", link: "/", icon: "ri-settings-line" },
-  ];
-  useEffect(() => {
-    if (!user) {
-      setOpen(true);
-    }
-  }, []);
   return (
     <>
       {user ? (
