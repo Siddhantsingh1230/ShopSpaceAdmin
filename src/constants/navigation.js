01266c3d211bb1df1434dd32c0b5e85abf89fcd6
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-23 12:18:16 +0530

    changed offers icon class

--- src/constants/navigation.js
+++ src/constants/navigation.js
@@ -11,6 +11,6 @@ export const navigation = [
     },
     { name: "Orders", link: "/orders", icon: "ri-archive-stack-line" },
     { name: "Products", link: "/products", icon: "ri-t-shirt-air-line" },
-    { name: "Offers", link: "/offers", icon: "ri-settings-line" },
+    { name: "Offers", link: "/offers", icon: "ri-price-tag-3-line" },
     { name: "Extras", link: "/extras", icon: "ri-box-3-line" },
   ];
