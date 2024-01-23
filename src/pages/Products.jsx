0d1a8477f60671670a1541987de6beb256c38940
khushi kandoi <khushikandoii09@gmail.com>
2024-01-23 19:54:08 +0530

    added product table

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,9 +1,42 @@
+import { AgGridReact } from "ag-grid-react";
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
+
 const Products = () => {
+  const columnDefs = [
+    {field : '_id'},
+    {field : 'title'},
+    {field : 'category'},
+    {field : 'subCategory'},
+    {field : 'brand'},
+    {field : 'price'},
+    {field : 'rating'},
+    {field : 'sale'},
+    {field : 'stock'},
+    {field : 'discountPercentage'},
+    {field : 'viewCount'},
+    {field : 'createdAt'},
+  ];
+
+  const defaultColDef = useMemo(()=>({
+    sortable : true,
+    filter : true,
+  }))
+
+  const products = useSelector((state)=>state.product.products);
   return (
     <>
-      Products
+      {/* product table */}
+      <div className="ag-theme-alpine-dark h-full">
+        <AgGridReact 
+        rowData={products}
+        columnDefs={columnDefs}
+        defaultColDef={defaultColDef}/>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Products;
