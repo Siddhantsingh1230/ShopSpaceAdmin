170565354496dc6d1abfaafeccf16e436d9911d0
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-25 23:57:01 +0530

    fixed multiple field update bug in products

--- src/components/DeleteModal.jsx
+++ src/components/DeleteModal.jsx
@@ -21,7 +21,7 @@ const DeleteModal = ({ open, setOpen, task, deleteItem}) => {
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-gray-950 bg-opacity-75 transition-opacity" />
         </Transition.Child>
 
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
