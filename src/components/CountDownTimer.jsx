0fadc537f336db936331311d63adad3c00073384
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-01-31 23:14:53 +0530

    added deal of day component in extras

---
+++ src/components/CountDownTimer.jsx
@@ -0,0 +1,57 @@
+import  { useEffect, useState } from 'react';
+
+const CountDownTimer = ({ seconds }) => {
+  const [time, setTime] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const countdownInterval = setInterval(() => {
+      const days = Math.floor(seconds / (3600 * 24));
+      const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+      const minutes = Math.floor((seconds % 3600) / 60);
+      const remainingSeconds = seconds % 60;
+
+      setTime({
+        days,
+        hours,
+        minutes,
+        seconds: remainingSeconds,
+      });
+
+      if (seconds === 0) {
+        clearInterval(countdownInterval);
+      } else {
+        seconds -= 1;
+      }
+    }, 1000);
+
+    return () => clearInterval(countdownInterval);
+  }, [seconds]);
+
+  return (
+    <div className="flex gap-5 max-sm:gap-2">
+      <div className="countdown-item bg-blue-500 py-2  flex justify-center items-center flex-col rounded-lg max-sm:px-4 flex-1">
+        <div className="text-md font-bold">{time.days}</div>
+        <div className="text-sm max-sm:text-xs">Days</div>
+      </div>
+      <div className="countdown-item bg-blue-500 py-2  flex justify-center items-center flex-col rounded-lg max-sm:px-4 flex-1">
+        <div className="text-md font-bold max-sm:text-sm">{time.hours}</div>
+        <div className="text-sm max-sm:text-xs">Hours</div>
+      </div>
+      <div className="countdown-item bg-blue-500 py-2  flex justify-center items-center flex-col rounded-lg max-sm:px-4 flex-1">
+        <div className="text-md font-bold max-sm:text-sm">{time.minutes}</div>
+        <div className="text-sm max-sm:text-xs">Min</div>
+      </div>
+      <div className="countdown-item bg-blue-500 py-2  flex justify-center items-center flex-col rounded-lg max-sm:px-4 flex-1">
+        <div className="text-md font-bold max-sm:text-sm">{parseInt(time.seconds)}</div>
+        <div className="text-sm max-sm:text-xs">Sec</div>
+      </div>
+    </div>
+  );
+};
+
+export default CountDownTimer;
--- src/components/DealOfTheDay.jsx
+++ src/components/DealOfTheDay.jsx
@@ -1,7 +1,87 @@
+import { useEffect, useState } from "react";
+import { getCurrentDeal } from "../api/dealOfTheDay.js";
+import CountDownTimer from "../components/CountDownTimer.jsx";
+import Stars from "../components/Stars.jsx";
+
 const DealOfTheDay = () => {
+  const [currentDeal, setCurrentDeal] = useState({});
+  const [offerDuration, setOfferDuration] = useState();
+  const fetchCurrentDeal = async () => {
+    try {
+      const { deal } = await getCurrentDeal();
+      setCurrentDeal(deal);
+      console.log(deal);
+      setOfferDuration((new Date(deal?.offerDuration) - new Date()) / 1000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCurrentDeal();
+  }, []);
   return (
     <>
-      <div className="h-full w-full text-white">Deal Of the Day</div>
+      <div className="w-full h-full py-5 md:p-5 text-white max-sm:justify-center max-sm:flex-col max-sm:items-center flex max-sm:gap-5 gap-5 rounded-md overflow-hidden">
+        {/* img */}
+        <div className="w-1/2 h-full max-sm:w-[90%] rounded-lg  p-2 border-2 border-[#4d71c5]">
+          <img
+            className="w-full h-full rounded-md  "
+            src={currentDeal?.productId?.thumbnail}
+            alt=""
+          />
+        </div>
+        {/* Details */}
+        <div className="w-1/2 h-full flex max-sm:w-full flex-col">
+          {/* Product title */}
+          <h1 className="text-xl text-white text-center mb-2">
+            {currentDeal?.productId?.title}
+          </h1>
+          {/* Rating/Stars */}
+          <div className="w-full justify-center items-center flex  mb-5">
+            <Stars
+              star={Math.round(parseInt(currentDeal?.productId?.rating || 0))}
+            />
+          </div>
+          {/* Timer */}
+          <div className="w-full px-20 max-sm:px-5 mb-10 max-sm:mb-5 ">
+            <CountDownTimer seconds={offerDuration || ""} />
+          </div>
+          
+          <hr className="border-t border-gray-800 mb-5" />
+          {/* Product details */}
+          <div className="flex  justify-center items-center gap-5 max-sm:mb-5 mb-10">
+            <div className="flex flex-col border-r border-[#4d71c5] flex-grow">
+              <span className="text-[#4d71c5] text-center max-sm:text-base font-bold text-2xl mb-2">
+                Price
+              </span>{" "}
+              <span className="text-white text-center font-bold  text-sm flex-grow">
+                ₹ {currentDeal?.productId?.price}
+              </span>
+            </div>
+            <div className="flex flex-col border-r border-[#4d71c5] flex-grow">
+              <span className="text-[#4d71c5] text-center max-sm:text-base font-bold text-2xl mb-2">
+                Brand
+              </span>{" "}
+              <span className="text-white text-center font-bold  text-sm flex-grow">
+                {currentDeal?.productId?.brand}
+              </span>
+            </div>
+            <div className="flex flex-col flex-grow">
+              <span className="text-[#4d71c5] text-center max-sm:text-base font-bold text-2xl mb-2">
+                Discount
+              </span>{" "}
+              <span className="text-white text-center font-bold  text-sm flex-grow">
+                {currentDeal?.productId?.discountPercentage}%
+              </span>
+            </div>
+          </div>
+          
+          
+          
+          
+        </div>
+      </div>
     </>
   );
 };
