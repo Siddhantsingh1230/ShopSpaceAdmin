e884d0429d94ea2c344a94d7cc402a28245aa1d5
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-02-05 11:41:08 +0530

    modified styles of kanbanboard

--- src/components/KanbanBoard/Task.jsx
+++ src/components/KanbanBoard/Task.jsx
@@ -14,11 +14,11 @@ export default function Task({ task, index, columnID }) {
               ? "bg-[#8f5af1] "
               : columnID == "2"
               ? "bg-[#e49771] "
-              : "bg-[#d77784] "
+              : "bg-[#fa51e3] "
           } rounded-md mb-5 grid grid-cols-11 px-2 py-5 `}
         >
-          <div className="col-span-1"><i className="ri-draggable text-black"></i></div>
-          <div className="col-span-9">{task.title}</div>
+          <div className="col-span-1 pl-2"><i className="ri-draggable text-black"></i></div>
+          <div className="col-span-9 text-sm ">{task.title}</div>
           <div title="delete" className="col-span-1 text-xl cursor-pointer "><i className="ri-close-fill"></i></div>
         </div>
       )}
