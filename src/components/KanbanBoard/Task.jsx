70e0f3132c97521caeb20ed5145968eebf7d3e5d
siddhantsingh1230 <siddhantsingh9023134084@gmail.com>
2024-02-05 12:28:51 +0530

    made kanbanboard responsive

--- src/components/KanbanBoard/Task.jsx
+++ src/components/KanbanBoard/Task.jsx
@@ -15,7 +15,7 @@ export default function Task({ task, index, columnID }) {
               : columnID == "2"
               ? "bg-[#e49771] "
               : "bg-[#fa51e3] "
-          } rounded-md mb-5 md:grid md:grid-cols-11 px-2 py-3 max-sm:py-1 max-sm:pb-3 max-sm:px-0 max-sm:flex max-sm:flex-col-reverse  `}
+          } rounded-md mb-5 max-sm:mb-3 md:grid md:grid-cols-11 px-2 py-3 max-sm:py-1 max-sm:pb-3 max-sm:px-0 max-sm:flex max-sm:flex-col-reverse  `}
         >
           <div className="md:col-span-1 pl-2 max-sm:hidden"><i className="ri-draggable text-black"></i></div>
           <div className="md:col-span-9 text-sm max-sm:text-xs max-sm:px-2 ">{task.title}</div>
